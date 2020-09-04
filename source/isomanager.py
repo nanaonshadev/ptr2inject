7f29e33ed395e00c3172ecbcad84c6c81f2859a2
jmkdev <jmk@jmksite.dev>
2020-09-04 10:05:19 -0400

    Wiped broken files

--- source/isomanager.py
+++ source/isomanager.py
@@ -1,9 +1,1 @@
-import pycdlib
-
-iso = pycdlib.PyCdlib()
-iso.open("D:\\jmkdrvbak\\Storage\\PaRappa the Rapper 2\\PTR2EDIT.iso")
-
-for child in iso.list_children(iso_path='/'):
-    print(child.file_identifier())
-
-iso.add_file("")
+print("nothing here yet")
